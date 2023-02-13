acec8654dab26eb82ad20753bee6ed3ed34c31d9
aligbillow <alibillow0@gmail.com>
2023-02-13 10:14:45 -0800

    added files

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,17 +1,26 @@
 import React, { Component } from 'react';
+import ParticlesBg from 'particles-bg';
 import Navigation from './components/navigation/navigation';
 import Logo from './components/Logo/Logo';
 import ImageLinkForm from './components/ImageLinkForm/ImageLinkForm';
-
-
+import Rank from './components/Rank/Rank';
 import './App.css';
+
+// const particlesOptions = {
+//   color= "#ff0000", 
+//   num= {200},
+//   type= "circle", 
+//   bg= {true} 
+// }
 
 class App extends Component {
   render() {
     return (
     <div className="App">
-      <Navigation />
+        {/* <div>...</div> */}
+        <ParticlesBg className="particles" num={150} color='#FFFFFF' type='cobweb' bg={true} />      <Navigation />
       <Logo />
+      <Rank />
       <ImageLinkForm />
       {/* {<FacialRecognition /> } */}
     </div>
