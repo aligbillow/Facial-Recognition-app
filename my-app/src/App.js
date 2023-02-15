ec95dc61ac489ce20aba5f96a38b201ba7011823
aligbillow <alibillow0@gmail.com>
2023-02-14 21:16:13 -0800

    added files

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,13 +1,20 @@
 import React, { Component } from 'react';
 import ParticlesBg from 'particles-bg';
+import Clarifai from 'clarifai';
+import FacialRecognition from './components/FacialRecognition/FacialRecognition';
 import Navigation from './components/navigation/navigation';
 import Logo from './components/Logo/Logo';
 import ImageLinkForm from './components/ImageLinkForm/ImageLinkForm';
 import Rank from './components/Rank/Rank';
 import './App.css';
 
+const app = new Clarifai.App({
+ apiKey: 'ac2424d97e6c4e30be8ef8c61ace084c'
+});
+
+
 class App extends Component {
-  constructor() { // functionality to search 
+  constructor() { 
     super();
     this.state = {
       input: '',
@@ -19,7 +26,17 @@ class App extends Component {
   } 
 
   onButtonSubmit = () => {
-    console.log('click')
+    console.log('click');
+    app.models.predict('53e1df302c079b3db8a0a36033ed2d15',
+     "https://samples.clarifai.com/face-det.jpg")
+     .then(
+      function(response) {
+         console.log(response);
+      },
+      function(err) {
+        console.log(err)
+      }
+    );
   }
 
   render() {
@@ -31,7 +48,7 @@ class App extends Component {
       <ImageLinkForm 
       onInputChange={this.onInputChange}
       onButtonSubmit={this.onButtonSubmit} />
-      {/* {<FacialRecognition /> } */}
+      <FacialRecognition /> 
     </div>
   );
   }
